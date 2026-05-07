adfa32f61e21e3000f4e39ab05f52d3b477b9474
aelfsige <eatuneytmiu@gmail.com>
2026-05-07 10:26:18 +0800

    Addedd and configured service worker

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker.register('/sw.js')
+        .then((reg) => console.log('service worker registered', reg))
+        .catch((err) => console.log('servive worker not registered', err))
+}
